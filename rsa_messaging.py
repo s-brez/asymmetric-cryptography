882f1cb9b6b189998edbd08c419a6addec00c7d7
s-brez <sam@sdbgroup.io>
2019-08-29 12:42:40 +1000

    Complete RSA text messaging scheme
    - used wolfram alpha to manually generate the signature for large number operations, python too slow

--- rsa_messaging.py
+++ rsa_messaging.py
@@ -5,31 +5,37 @@ import crypto_functions as cf
 
 # RSA text message signature scheme ##################################
 
-# message = "Cryptocurrencies continue to grow in price and size. Knowledge about Bitcoin, Litecoin, Ethereum, and others has spread through the entire world. Cryptocurrencies are providing such features and tools that simplify our lives. They are changing the way things work. Some people fear the changes. But changes are not always bad. Cryptocurrencies are modifying our lives, and the way industries develop. There’s no doubt that cryptocurrencies are disrupting and affecting the global economy in many ways."
-message = "Security in Computing 2018"
+message = "Cryptocurrencies continue to grow in price and size. Knowledge about Bitcoin, Litecoin, Ethereum, and others has spread through the entire world. Cryptocurrencies are providing such features and tools that simplify our lives. They are changing the way things work. Some people fear the changes. But changes are not always bad. Cryptocurrencies are modifying our lives, and the way industries develop. There’s no doubt that cryptocurrencies are disrupting and affecting the global economy in many ways."
 print("Message:", message)
 
 # Sender hashes the message then converts to decimal (from hex)
-# msg_hash = hashlib.md5(message.encode('utf-8')).hexdigest()
-msg_hash = "db1623867ac43b924848c0bc81a6e689"
-print("MD5 Hash:", msg_hash)
+msg_hash = hashlib.md5(message.encode('utf-8')).hexdigest()
+print("MD5 message Hash:", msg_hash)
 
 msg_hash = int(msg_hash, 16)
-print("As decimal:", msg_hash)
+print("Hash as decimal:", msg_hash)
 
 # Sender generates keys
-p = 335011793073035265521070150212791157303
-q = 185296104977565236504132463330798131651
-e = 5737
+p = 278966591577398076867954212605012776073
+q = 467207331195239613378791200749462989467
+e = 41
 keys = rsa.generate_keys(p, q, e)
 print("Keys:", keys)
 
 # Sender signs the message with the private key
-s = rsa.sign(keys['n'], keys['d'], msg_hash)
+# s = rsa.sign(keys['n'], keys['d'], msg_hash)
+
+# Use wolfram alpha to sign (m^d mod n), python slow for large number operations
+s = 126798804286385130870848966135941566606057839336135951340495096277825470279796
 print("Signature:", s)
 
-# receiver verifies the signature and message
-verified = rsa.verify_signature(keys['n'], keys['e'], s, msg_hash)
-print("Message matches signature:", verified)
+# Receiver verifies the signature and message
+verify = rsa.verify_signature(keys['n'], keys['e'], s, msg_hash)
+print("Message matches signature:", verify)
 
 # Receiver hashes the original message to check it matches the given hash 
+rehashed_msg = hashlib.md5(message.encode('utf-8')).hexdigest()
+print("Re-hashed message:", rehashed_msg)
+print(
+	"Original message hash matches senders second hashing:", 
+	int(rehashed_msg, 16) == msg_hash)
